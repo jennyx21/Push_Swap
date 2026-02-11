a5b2274e9fc92278fa26031ea54ce861cf65fb5b
jennyx21 <jtruckse@student.42heilbronn.de>
2026-02-11 13:50:48 +0100

    implement input validation

--- src/validate_input.c
+++ src/validate_input.c
@@ -6,25 +6,30 @@
 /*   By: jtruckse <jtruckse@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 16:27:24 by jtruckse          #+#    #+#             */
-/*   Updated: 2026/02/10 18:09:27 by jtruckse         ###   ########.fr       */
+/*   Updated: 2026/02/11 13:49:39 by jtruckse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+t_stack *ft_newnode( int num );
+int check_if_valid_num(char *str);
+void ft_make_stack(t_stack **stack, int number);
+t_stack *ft_newnode( int num );
 
-int	validate_input(int ac, char **av)
+t_stack	**validate_input(int ac, char **av)
 {
-	int		i;
-	int		j;
-	long	num;
-	char	**numbers;
-
-
-	i = 0;
+	int				i;
+	int				j;
+	long			num;
+	char			**numbers;
+	struct t_stack 	*stack_a;
+	
+	stack_a = NULL;
 	j = 1;
 	while(ac-- > 1)
 	{
 		numbers = ft_split(*av[j], find_witespace(*av[j]));
+		i = 0;
 		while(numbers[i] != NULL)
 		{
 			if(check_if_valid_num(numbers[i]) == 0)
@@ -32,10 +37,12 @@ int	validate_input(int ac, char **av)
 			num = ft_atol(numbers[i]);
 			if(num < INT_MIN || num > INT_MAX)
 				return(write(1, "Error\n", 6));
+			ft_make_stack(&stack_a, num);
 			i++;
 		}
 	j++;
 	}	
+	return (stack_a);
 }
 int check_if_valid_num(char *str)
 {
@@ -53,4 +60,41 @@ int check_if_valid_num(char *str)
 		i++;
 	}
 	return(1);
+}
+void ft_make_stack(t_stack **stack, int number)
+{
+	t_stack *new; 
+	t_stack *temp;
+
+	new = ft_newnode(number);
+	
+	if (*stack == NULL)
+	{
+		*stack = new;
+	}
+	temp = *stack;
+	while (temp ->next != 0)
+		temp = temp ->next;
+	temp -> next = new;
+}
+
+t_stack *ft_newnode( int num )
+{
+	t_stack *new;
+
+	new = malloc(sizeof(t_stack));
+	if (!new)
+		return(0);
+	new->data = num;
+	new->next =NULL;
+	return(new);		
+}
+int main(int argc, char **argv)
+{
+	t_stack **list;
+	int i = 0;
+
+	list = validate_input(argc, argv);
+	while(*list -> next != NULL );
+	
 }
